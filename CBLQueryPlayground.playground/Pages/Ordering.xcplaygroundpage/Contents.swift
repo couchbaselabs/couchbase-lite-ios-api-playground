4c2df55215eac8460d2c094eda282e0131908c8f
rajagp <r7.priya@gmail.com>
2017-11-04 10:41:43 -0400

    ordering example

--- CBLQueryPlayground.playground/Pages/Ordering.xcplaygroundpage/Contents.swift
+++ CBLQueryPlayground.playground/Pages/Ordering.xcplaygroundpage/Contents.swift
@@ -1,7 +1,109 @@
-//: [Previous](@previous)
+/*:
+ [Table of Contents](ToC) | [Previous](@previous) | [Next](@next)
+ ****
+ 
+ Example that sorts the results of a query
 
+ 
+ */
+
+import UIKit
+import CouchbaseLiteSwift
 import Foundation
+import PlaygroundSupport
 
-var str = "Hello, playground"
+/*:
+ ## Definition of a Document object returned by the Couchbase Lite query. Note that in an actual application, you would probably define a native object instead of a generic map type of the kind defined below
+ 
+ */
 
-//: [Next](@next)
+typealias Data = [String:Any?]
+/*:
+ ## Opens Couchbase Lite Database.
+ The opens the database from prebuilt travel-sample database in `playgroundSharedDataDirectory`. Make sure that you have the "travel-sample.cblite2" folder copied over to the ~/Documents/Shared\ Playground\ Data/ folder
+ - returns: Handle to CBLite database
+ - throws exception if failure to create/open database
+ 
+ */
+func createOrOpenDatabase() throws -> Database? {
+    let sharedDocumentDirectory = playgroundSharedDataDirectory.resolvingSymlinksInPath()
+    let kDBName:String = "travel-sample"
+    let fileManager:FileManager = FileManager.default
+    
+    var options =  DatabaseConfiguration()
+    let appSupportFolderPath = sharedDocumentDirectory.path
+    options.fileProtection = .noFileProtection
+    options.directory = appSupportFolderPath
+    
+    // Uncomment the line below  if you want details of the SQLite query equivalent
+    // Database.setLogLevel(.verbose, domain: .all)
+    return try Database(name: kDBName, config: options)
+    
+    
+}
+
+/*:
+ ## Close database
+ - parameter db : The database to close
+ - throws exception if failure to close
+ */
+
+func closeDatabase(_ db:Database) throws  {
+    try db.close()
+}
+
+/*:
+ ## Query for documents sorting results in ascending order of a specific property
+ - parameter db : The database to query
+ - parameter limit: The max number of documents to fetch. Defaults to 10
+ - returns: Documents matching the query
+ 
+ */
+
+func queryForDocumentsInAscendingOrderFromDB(_ db:Database, limit:Int = 10 ) throws -> [Data]? {
+    
+    let searchQuery = Query
+        .select(
+            SelectResult.expression(Expression.meta().id),
+            SelectResult.expression(Expression.property("title")))
+        .from(DataSource.database(db))
+        .where(Expression.property("type").equalTo("hotel"))
+        .orderBy(Ordering.property("title").ascending())
+        .limit(limit)
+    
+    var matches:[Data] = [Data]()
+    do {
+        for row in try searchQuery.run() {
+            matches.append(row.toDictionary())
+        }
+    }
+    return matches
+}
+
+
+/*:
+ ## Run the queries defined in the above functions
+ */
+
+
+do {
+    // Open or Create Couchbase Lite Database
+    if let db:Database = try createOrOpenDatabase() {
+        
+        let results1 = try queryForDocumentsInAscendingOrderFromDB(db, limit: 5)
+        print(results1)
+        
+       
+        // try closeDatabase(db)
+    }
+    
+}
+catch {
+    print ("Exception is \(error.localizedDescription)")
+}
+
+
+
+
+
+
