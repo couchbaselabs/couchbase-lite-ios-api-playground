e9e59fb466d50e85889f7991ebfd92bbb419417c
rajagp <r7.priya@gmail.com>
2018-01-22 10:39:45 -0500

    JOIN query examples

--- CBLQueryPlayground.playground/Pages/Joins.xcplaygroundpage/Contents.swift
+++ CBLQueryPlayground.playground/Pages/Joins.xcplaygroundpage/Contents.swift
@@ -178,7 +178,119 @@ func queryForDocumentsFromDatabasePerformingLeftJoin(_ db:Database) throws -> [D
     return matches
 }
 
+/*:
+ ## Do an cross join on "department" type documents with "location" type documents
+ The "name" properties from "department" document are returned along with the
+ location "name"  and "address" from the "location" document.
+ 
+ - parameter db : The database to query
+ - returns: Documents matching the query
+ 
+ */
 
+func queryForDocumentsFromDatabasePerformingCrossJoin(_ db:Database) throws -> [Data]? {
+    // join key cannot be missing. cannot hsve employee doc with department missing
+    let departmentDS = DataSource.database(db).as("departmentDS")
+    let locationDS = DataSource.database(db).as("locationDS")
+    
+    
+    
+    // join expression
+    let join = Join.crossJoin(locationDS)
+    
+    // type property expressions
+    let deptTypeExpr = Expression.property("type").from("departmentDS")
+    let locationTypeExpr = Expression.property("type").from("locationDS")
+    
+    let searchQuery = Query.select(SelectResult.expression(Expression.property("name").from("departmentDS")).as("DeptName"),
+                                   SelectResult.expression(Expression.property("name").from("locationDS")).as("LocationName"),
+                                   SelectResult.expression(Expression.property("address").from("locationDS")))
+        .from(departmentDS)
+        .join(join)
+        .where(deptTypeExpr.equalTo(Expression.string("department"))
+                    .and(locationTypeExpr.equalTo(Expression.string("location"))))
+    
+    
+    
+    // print(try searchQuery.explain())
+    
+    var matches:[Data] = [Data]()
+    do {
+        for row in try searchQuery.execute() {
+            
+            let r = row.toDictionary()
+            print(r)
+            
+            matches.append(row.toDictionary())
+        }
+    }
+    return matches
+}
+
+
+/*:
+ ## Do an left Join on "employee" type documents with "department" type documents based
+ on the department code and also a left join on "employee" type documents with "location" type documents
+ based on location code. In this case, even employee documents that have no departments or locations are returned
+ The "firstname", "lastname" properties from "employee" document are returned along with the
+ department "name" from the "department" document.
+ 
+ - parameter db : The database to query
+ - returns: Documents matching the query
+ 
+ */
+
+func queryForDocumentsFromDatabasePerformingLeftJoinOnThreeDocuments(_ db:Database) throws -> [Data]? {
+    // join key cannot be missing. cannot hsve employee doc with department missing
+    let employeeDS = DataSource.database(db).as("employeeDS")
+    let departmentDS = DataSource.database(db).as("departmentDS")
+    let locationDS = DataSource.database(db).as("locationDS")
+    
+    let employeeDeptExpr = Expression.property("department").from("employeeDS")
+    let employeeLocationExpr = Expression.property("location").from("employeeDS")
+    let departmentCodeExpr = Expression.property("code").from("departmentDS")
+    let locationCodeExpr = Expression.property("code").from("locationDS")
+    
+    // Join where the "department" field of employee documents is equal to the department "code" of
+    // "department" documents
+    let joinDeptCodeExpr = employeeDeptExpr.equalTo(departmentCodeExpr)
+        .and(Expression.property("type").from("employeeDS").equalTo(Expression.string("employee")))
+        .and(Expression.property("type").from("departmentDS").equalTo(Expression.string("department")))
+    
+    // Join where the "department" field of employee documents is equal to the department "code" of
+    // "department" documents
+    let joinLocationCodeExpr = employeeLocationExpr.equalTo(locationCodeExpr)
+        .and(Expression.property("type").from("employeeDS").equalTo(Expression.string("employee")))
+        .and(Expression.property("type").from("locationDS").equalTo(Expression.string("location")))
+    
+    // join expression for department code
+    let joinDeptCode = Join.join(departmentDS).on(joinDeptCodeExpr)
+    
+    // join expression for location code
+    let joinLocationCode = Join.join(locationDS).on(joinLocationCodeExpr)
+    
+    let searchQuery = Query.select(SelectResult.expression(Expression.property("firstname").from("employeeDS")),
+                            SelectResult.expression(Expression.property("lastname").from("employeeDS")),
+                            SelectResult.expression(Expression.property("name").from("departmentDS")).as("deptName"),
+                            SelectResult.expression(Expression.property("name").from("locationDS")).as("locationName"))
+        .from(employeeDS)
+        .join(joinDeptCode,joinLocationCode)
+    
+    
+    // print(try searchQuery.explain())
+    
+    var matches:[Data] = [Data]()
+    do {
+        for row in try searchQuery.execute() {
+            
+            let r = row.toDictionary()
+            print(r)
+            
+            matches.append(row.toDictionary())
+        }
+    }
+    return matches
+}
 
 /*:
  ## Run the queries defined in the above functions
@@ -191,9 +303,15 @@ do {
         
 //        let results1 = try queryForDocumentsFromDatabasePerformingInnerJoin(db)
 //        print("\n*****\nResponse to queryForDocumentsFromDatabasePerformingInnerJoin :\n\(results1)")
+//
+//        let results2 = try queryForDocumentsFromDatabasePerformingLeftJoin(db)
+//        print("\n*****\nResponse to queryForDocumentsFromDatabasePerformingLeftJoin :\n\(results2)")
 
-        let results2 = try queryForDocumentsFromDatabasePerformingLeftJoin(db)
-        print("\n*****\nResponse to queryForDocumentsFromDatabasePerformingLeftJoin :\n\(results2)")
+//        let results3 = try queryForDocumentsFromDatabasePerformingCrossJoin(db)
+//        print("\n*****\nResponse to queryForDocumentsFromDatabasePerformingCrossJoin :\n\(results3)")
+
+        let results4 = try queryForDocumentsFromDatabasePerformingLeftJoinOnThreeDocuments(db)
+        print("\n*****\nResponse to queryForDocumentsFromDatabasePerformingLeftJoinOnThreeDocuments :\n\(results4)")
 
         // try closeDatabase(db)
     }
